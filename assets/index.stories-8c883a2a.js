c642916729bcdffe2f6eed56f17fce8bb632d179
DavideBruner <davidebruner@hotmail.it>
2022-12-28 17:55:57 +0000

    Deploying to docs from  @ b385ee6f33c23d0f143e374b9bbb6382ac0a2568 🚀

---
+++ assets/index.stories-8c883a2a.js
@@ -0,0 +1,2 @@
+var e=Object.defineProperty;var o=(t,n)=>e(t,"name",{value:n,configurable:!0});import{j as s}from"./jsx-runtime-c368ffdb.js";import"./index-7cc2572b.js";import"./es.object.get-own-property-descriptor-3a7d61f2.js";const r=o(({children:t})=>s("button",{children:t}),"Button"),a=r,m={title:"Components/Button",component:a,args:{children:"Create account"},argTypes:{}},d={},l=["Default"];export{d as Default,l as __namedExportsOrder,m as default};
+//# sourceMappingURL=index.stories-8c883a2a.js.map
